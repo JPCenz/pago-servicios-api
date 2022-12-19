e88a684b168493e5d0190de104d1a1a990cf25c3
JPCenz <p.cenzano5@gmail.com>
2022-12-19 13:46:52 -0500

    agrego expiration_date +30 dias de la fecha de pago en model PaymentUser

--- api_pagos/v1/serializers.py
+++ api_pagos/v1/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework.serializers import ModelSerializer,StringRelatedField
 from .. import models
+import datetime
 
 class ServiceSerializer(ModelSerializer):
     class Meta:
@@ -7,12 +8,18 @@ class ServiceSerializer(ModelSerializer):
         fields= "__all__"
 
 class PaymentUserSerializer(ModelSerializer):
-    service = StringRelatedField(many=False)
+    # service = StringRelatedField(many=False,read_only=False)
     class Meta:
         model = models.PaymentUser
         fields= "__all__"
+        # read_only_fields= ("expiration_date",)
+    def create(self, validated_data:dict):
+        p = validated_data.get("payment_date")
+        validated_data["expiration_date"]= p + datetime.timedelta(days=30)
+        payment = super().create(validated_data)
         
-        # read_only_fields= ("user",)
+        return payment
+        
 
 class ExpiredPayments(ModelSerializer):
     class Meta:
